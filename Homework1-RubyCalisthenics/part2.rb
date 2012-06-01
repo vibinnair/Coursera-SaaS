ddade059e65fa162e55c7310c4ea74ef06499639
vibin.nair@gmail.com <vibin.nair@gmail.com>
2012-06-01 22:48:34 +0530

    Completed Homework 1 - Part 2

--- Homework1-RubyCalisthenics/part2.rb
+++ Homework1-RubyCalisthenics/part2.rb
@@ -3,25 +3,37 @@ class NoSuchStrategyError < StandardError ; end
 
 def rps_game_winner(game)
 	raise WrongNumberOfPlayersError unless game.length == 2
-	raise NoSuchStrategyError unless game[0][1] =~  /[PSR]/i && game[1][1] =~ /[PSR]/i
 	
-	game[0][1] if game[0][1] == 'P' && game[1][1] == 'R' #but then what if [0][1] is R and [1][1] is P
+	player1 = game[0]
+	player2 = game[1] 	
+	player1_strategy = game[0][1]
+	player2_strategy = game[1][1]
+	#list of against whom can a person loose. i.e s can only loose against r, p can only loose against s and r can only loose against p  
+	can_only_loose_against_rule = {:s => :r, :p => :s, :r => :p} 
 	
-	 		
+	raise NoSuchStrategyError unless player1_strategy =~  /[PSR]/i && player2_strategy =~ /[PSR]/i
 	
+	if(player1_strategy == player2_strategy)
+		player1
+	elsif (player1_strategy.downcase.to_sym == can_only_loose_against_rule[player2_strategy.downcase.to_sym]) #Checking if player1 is the person against whom player2 can loose  
+		player1
+	else
+		player2	
+	end  	
 end	
 
-rps_game_winner([ [ "Armando", "S" ], [ "Dave", "P" ] ])
 
-
-
-
-#	game.each do |player|
-#		 	raise NoSuchStrategyError unless player[1] =~ /[PSR]/i
-#	end
-
-
-#winner = game.select { |player|
-#					
-#			 }
-#	print winner
+def rps_tournament_winner(tournament)
+	
+	while true do
+		tournament	= tournament.flatten
+	
+		if(tournament.length > 4) then
+			tournament = tournament.each_slice(2).each_slice(2).to_a
+			tournament = tournament.map{|game| rps_game_winner(game)}
+		else
+			game = tournament.each_slice(2).to_a
+			return rps_game_winner(game)
+		end
+	end			
+end	
