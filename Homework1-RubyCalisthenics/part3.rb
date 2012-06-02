d9df5042c31c875f08a7f011998368d2d36d6330
vibinnair <vibin.nair@gmail.com>
2012-06-02 20:57:13 +0530

    Completed Homework-1 part 3

--- Homework1-RubyCalisthenics/part3.rb
+++ Homework1-RubyCalisthenics/part3.rb
@@ -1,7 +1,22 @@
 def combine_anagrams(words)
+	actual_words = words
 	sorted_words = words.map{|word| word.chars.sort.join}
+
+	anagrams_map={}
+		
+	# create a map in which key will be the sorted word and value will be array of words that sort to form the sorted word.
+	# i.e {"acrs" => ["cars", "racs","scar"], "for" => [for], "aeoopstt" => ["potatoes"], "foru" => ["four"]}
+	sorted_words.each_with_index do |word, index|
+        if anagrams_map.has_key?(word)
+			anagrams_map[word] << actual_words[index]
+		else	 	
+			anagrams_map[word] = Array.new
+			anagrams_map[word] << actual_words[index]
+		end	
+	end
+	
+	anagrams_map.values.inject([]){|list,| list<<value}
+	
 end
 
-combine_anagrams(['cars', 'for', 'potatoes', 'racs', 'four','scar', 'creams', 'scream'])
-
-["acrs", "for", "aeoopstt", "acrs", "foru", "acrs", "acemrs", "acemrs"]
+# p combine_anagrams(['cars', 'for', 'potatoes', 'racs', 'four','scar', 'creams', 'scream'])
